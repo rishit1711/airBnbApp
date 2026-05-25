56f80ac7b89ad5b615fcb9878a6589055db88297
Rishit Roy <hellorishit.roy@gmail.com>
2026-05-26 00:02:05 +0530

    Initial commit

---
+++ src/main/java/com/example/Project_Rishit/airBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_Rishit.airBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
